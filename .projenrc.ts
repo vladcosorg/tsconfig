524e41b64b20b9bc8ff3805c93ce84f4f11bcbc5
Vlad Cos <vcosvic@gmail.com>
2023-06-15 15:03:38 +0300

    WIP

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,23 @@
 import * as path from 'node:path'
 
-import { CustomTypescriptProject } from '@vladcos/projen-base'
+import { TypeScriptProject } from '@vladcos/projen-base'
 
-const project = new CustomTypescriptProject({
-  defaultReleaseBranch: 'main',
-  devDeps: ['@vladcos/projen-base'],
-  name: '@vladcos/tsconfig',
-  projenrcTs: true,
-  entrypoint: `lib/tsconfig.json`,
-  tsconfigTemplatePath: path.resolve('./src/tsconfig.json'),
-})
-project.compileTask.reset(`cp -R src  ${project.libdir}`)
+const project = new (class extends TypeScriptProject {
+  constructor() {
+    super({
+      defaultReleaseBranch: 'main',
+      devDeps: ['@vladcos/projen-base'],
+      name: '@vladcos/tsconfig',
+      projenrcTs: true,
+      entrypoint: `lib/tsconfig.json`,
+      tsconfigTemplatePath: path.resolve('./src/tsconfig.json'),
+    })
+  }
+
+  override preSynthesize() {
+    super.preSynthesize()
+    project.compileTask.reset(`cp -R src  ${project.libdir}`)
+  }
+})()
+
 project.synth()
